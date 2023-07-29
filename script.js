bd3b6787bb02faf23655c9b04cc0020c3dbbafe8
Lucas Rodrigues Cunha <123827882+LucasRodriguesCunha@users.noreply.github.com>
2023-07-29 10:20:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const botaoSubir = document.querySelector("#voltar-ao-topo");
 
 window.addEventListener("scroll", function() {
     const scrollTop = document.documentElement.scrollTop;
-
+    
     if (scrollTop > 500)
         botaoSubir.style.display = "block";
     else {
